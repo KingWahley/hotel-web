af212f1f2c9b6bcce8b18e5ba26cf48e0710ff45
KingWahley <kingwahley@gmail.com>
2026-01-19 11:12:03 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Nav from '../components/nav'
+import Header from '../components/header'
+import DreamHome from '../components/dream-home'
+import About from '../components/about'
+import Service from '../components/service'
+import Footer from '../components/footer'
+import OurRooms from '../components/our-rooms'
+import { Divider } from '@chakra-ui/core'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <DreamHome />
+      <About />
+      <Service />
+      <OurRooms />
+      <Divider mt="24" />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
